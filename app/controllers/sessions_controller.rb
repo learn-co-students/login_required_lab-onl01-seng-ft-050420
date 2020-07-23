c393fbfe46d39566872223525b7296a97e218a13
anette87 <anettem.rivera@gmail.com>
2020-07-23 14:28:21 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        if params[:name].empty?
+            redirect_to '/sessions/new'
+        else
+            session[:name] = params[:name]
+        end
+    end  
+  
+  
+    def destroy
+      session.delete :name 
+    end
+   
+
+end
